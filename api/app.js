036ab0d7487a947d3eecd154c146a08fd5ebff87
masozan7 <fauzan.fadilah@epfootwear.com>
2025-01-06 11:24:55 +0700

    Add Dockerfile, package.json, and app.js for API service

---
+++ api/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+      res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+      res.json({ message: 'Welcome to the API' });
+});
+
+app.get('/book', (req, res) => {
+      res.json({ 'id': 1, 'title': 'The Great Gatsby' });
+});
+
+app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+});
+app.get('/books', (req, res) => {
+      res.json([
+            { 'id': 1, 'title': 'The Great Gatsby' },
+            { 'id': 2, 'title': 'To Kill a Mockingbird' },
+            { 'id': 3, 'title': '1984' },
+            { 'id': 4, 'title': 'Pride and Prejudice' },
+            { 'id': 5, 'title': 'The Catcher in the Rye' }
+      ]);
+});
